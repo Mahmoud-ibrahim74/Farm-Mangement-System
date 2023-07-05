09a1142936670ea4ede840c1aa8c5f2af15bced0
Mahmoud Ibrahim <130683686+Mahmoud-ibrahim74@users.noreply.github.com>
2023-07-05 16:51:18 +0300

    uncomment

--- FMS/Program.cs
+++ FMS/Program.cs
@@ -9,7 +9,7 @@ namespace FMS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new MainMenu());
+            Application.Run(new MainMenu());
 
         }
     }
